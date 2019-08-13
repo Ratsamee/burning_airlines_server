6fb099b662e4522dce1618e1d16b2be8a35cd442
Harrison Malouf <harrisonmalouf@gmail.com>
2019-08-14 09:37:09 +1000

    seed data

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+u1 = Airplane.create :name => 'Freddie', :rows => 1 , :columns => 1
+
+u2 = Airplane.create :name => 'Garry', :rows => 2, :columns => 3
+
+u3 = Airplane.create :name => 'MEE', :rows => 3, :columns => 2
+
+u4 = Airplane.create :name => 'Turkey', :rows => 3, :columns => 1
+
+u5 = Airplane.create :name => 'Chicken', :rows => 6, :columns => 1
+
+u6 = Airplane.create :name => 'Bacon', :rows => 4, :columns => 2
